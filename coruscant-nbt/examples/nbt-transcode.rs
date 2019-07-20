afcb24e47d8a3cc9bf3db5f1789eb81e115950ca
luojia65 <me@luojia.cc>
2019-07-20 23:02:30 +0800

    Transcode example

--- coruscant-nbt/examples/nbt-transcode.rs
+++ coruscant-nbt/examples/nbt-transcode.rs
@@ -1,12 +1,13 @@
 use std::io::{Cursor, Write};
 
 fn main() {
-    let str_input = "{\"testByte\":127}";
+    let str_input = "{\"nick\":\"luojia65\", 
+        \"school\":{\"name\":\"hust\",\"985\":true}}";
     let vec_output = Vec::new();
     let write = Cursor::new(vec_output);
 
     let mut deserializer = serde_json::Deserializer::from_str(str_input);
-    let mut serializer = coruscant_nbt::Serializer::transcript(write, "");
+    let mut serializer = coruscant_nbt::Serializer::transcript(write, "person");
 
     serde_transcode::transcode(&mut deserializer, &mut serializer)
         .expect("serde transcode");

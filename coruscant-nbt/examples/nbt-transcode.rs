ba8a38b9d3e69fd5b2f5250a1a15789b72ad4ad8
luojia65 <me@luojia.cc>
2019-07-21 10:39:58 +0800

    Notes on transcode

--- coruscant-nbt/examples/nbt-transcode.rs
+++ coruscant-nbt/examples/nbt-transcode.rs
@@ -1,3 +1,14 @@
+// 20 lines of code that converts your JSON string into NBT format.
+//
+// This piece of code performs the transcode through `serde_transcode` crate. 
+// It prints NBT transcript for human readbility using `Serializer::transcript`, 
+// If you want NBT bytes, consider using `Serializer::binary` instead.
+// 
+// Due to the loss of number accuracy in JSON, this program only support limited
+// input data type from JSON, for example objects and strings. However, if we do
+// contrarily from NBT to JSON, full transcode support is possibleas the former 
+// does record all the data types for numbers.
+
 use std::io::{Cursor, Write};
 
 fn main() {
@@ -13,8 +24,9 @@ fn main() {
         .expect("serde transcode");
     
     let mut write = serializer.into_inner();
-    write.flush().unwrap();
+    write.flush().unwrap(); // Cursor flush never fails. If other `io::Write`
+                            // output is used, handle flush result properly.
     let string_output = String::from_utf8(write.into_inner()).unwrap();
-
+        // Transcript byte output from `coruscant_nbt` is always valid for utf8.
     println!("{}", string_output);
 }

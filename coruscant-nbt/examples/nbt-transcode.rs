b2396444041b37e55fb344bf49342546f68c357f
luojia65 <me@luojia.cc>
2019-07-22 17:52:06 +0800

    20 lines -> 5 lines

--- coruscant-nbt/examples/nbt-transcode.rs
+++ coruscant-nbt/examples/nbt-transcode.rs
@@ -1,4 +1,4 @@
-// 20 lines of code that converts your JSON string into NBT format.
+// 5 lines of code that converts your JSON string into NBT format.
 //
 // This piece of code performs the transcode through `serde_transcode` crate.
 // It prints NBT transcript for human readbility using `Serializer::transcript`;
@@ -9,23 +9,12 @@
 // contrarily from NBT to JSON, full transcode support is possible as the former
 // does record all the data types for numbers.
 
-use std::io::{Cursor, Write};
-
 fn main() {
-    let str_input = "{\"nick\":\"luojia65\", 
-        \"school\":{\"name\":\"hust\",\"985\":true}}";
-    let vec_output = Vec::new();
-    let write = Cursor::new(vec_output);
-
+    use std::io;
+    
+    let str_input = r#"{"nick":"luojia65","school":{"name":"hust","985":true}}"#;
     let mut deserializer = serde_json::Deserializer::from_str(str_input);
-    let mut serializer = coruscant_nbt::Serializer::transcript(write, "person");
+    let mut serializer = coruscant_nbt::Serializer::transcript(io::stdout(), "person");
 
     serde_transcode::transcode(&mut deserializer, &mut serializer).expect("serde transcode");
-
-    let mut write = serializer.into_inner();
-    write.flush().unwrap(); // Cursor flush never fails. If other `io::Write`
-                            // output is used, handle flush result properly.
-    let string_output = String::from_utf8(write.into_inner()).unwrap();
-    // Transcript byte output from `coruscant_nbt` is always valid for utf8.
-    println!("{}", string_output);
 }

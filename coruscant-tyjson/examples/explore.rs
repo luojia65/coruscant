fed6635e5feccc105917c092104a0c84f1166b88
luojia65 <me@luojia.cc>
2019-08-21 17:42:28 +0800

    Cross u64 boundary check exampkle

--- coruscant-tyjson/examples/explore.rs
+++ coruscant-tyjson/examples/explore.rs
@@ -11,13 +11,16 @@ fn main() {
         input_vec[i] = unsafe { _mm256_load_si256(ptr as *const _) };
         unsafe { ptr = ptr.add(32) };
     }
-    for input in &input_vec {
-        print_m256(*input)
-    }
-    odd_backslash_sequences(input_vec);
+    // for input in &input_vec {
+    //     print_m256(*input)
+    // }
+    let mut prev_ends_odd_backslash = unsafe { _mm256_setzero_si256() };
+    odd_backslash_sequences(input_vec, &mut prev_ends_odd_backslash);
+
+    println!("AVX2 {}", is_x86_feature_detected!("avx2"));
 }
 
-fn odd_backslash_sequences(input: [__m256i; 8]) -> __m256i {
+fn odd_backslash_sequences(input: [__m256i; 8], prev_ends_odd_backslash: &mut __m256i) -> __m256i {
     let even_bits = unsafe { _mm256_set1_epi8(transmute(0x55u8)) };
     let odd_bits = unsafe { _mm256_set1_epi8(transmute(0xAAu8)) };
 
@@ -34,17 +37,32 @@ fn odd_backslash_sequences(input: [__m256i; 8]) -> __m256i {
             _mm256_movemask_epi8(_mm256_cmpeq_epi8(input[0], mask)),
         )
     };
-    print_m256(backslashes);
+    // print_m256(backslashes);
 
     // let starts = backslashes & (!(backslashes << 1));
     // there is no slli for si256, have to split into 64 bit values
     let starts = unsafe {
         _mm256_andnot_si256(_mm256_slli_epi64(backslashes, 1), backslashes)
     };
-    print_m256(starts);
+    // print_m256(starts);
 
-    let even_starts = unsafe { _mm256_xor_si256(starts, even_bits) };
-    print_m256(even_starts);
+    //
+    unsafe {
+        print_m256(backslashes);
+        // let sll = _mm256_slli_epi64(backslashes, 1);
+        // print_m256(sll);
+        // let hi = _mm256_srli_epi64(backslashes, 63);
+        // print_m256(hi);
+        let ans = _mm256_permute4x64_epi64(backslashes, 0b00011011);
+        print_m256(ans);
+    }
+    //
+
+    let even_starts = unsafe { _mm256_andnot_si256(odd_bits, starts) };
+    // print_m256(even_starts);
+
+    let even_carries = unsafe { _mm256_add_epi64(even_starts, backslashes) };
+    // print_m256(even_carries);
 
     unimplemented!()
 }

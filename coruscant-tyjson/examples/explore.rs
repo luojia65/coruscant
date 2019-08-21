1a3ecc718f2030d81fb6481473249a0b026d3853
luojia65 <me@luojia.cc>
2019-08-21 12:07:11 +0800

    backslash

--- coruscant-tyjson/examples/explore.rs
+++ coruscant-tyjson/examples/explore.rs
@@ -1,12 +1,34 @@
 use std::arch::x86_64::*;
+use core::mem::transmute;
 
 fn main() {
+    let input = include_bytes!("explore.in");
     
 }
 
-fn odd_backslash_sequences(input: __m256i, prev_end: &mut u64) -> u64 {
-    const EVEN_BITS: u64 = 0x5555_5555_5555_5555;
-    const ODD_BITS: u64 = 0xAAAA_AAAA_AAAA_AAAA;
+fn odd_backslash_sequences(input: [__m256i; 8], prev: &mut __m256i) -> __m256i {
+    let even_bits = unsafe { _mm256_set1_epi8(transmute(0x55u8)) };
+    let odd_bits = unsafe { _mm256_set1_epi8(transmute(0xAAu8)) };
 
-    
+    let backslashes = unsafe {
+        let mask = _mm256_set1_epi8(b'\'' as i8);
+        _mm256_set_epi32(
+            _mm256_movemask_epi8(_mm256_cmpeq_epi8(input[7], mask)),
+            _mm256_movemask_epi8(_mm256_cmpeq_epi8(input[6], mask)),
+            _mm256_movemask_epi8(_mm256_cmpeq_epi8(input[5], mask)),
+            _mm256_movemask_epi8(_mm256_cmpeq_epi8(input[4], mask)),
+            _mm256_movemask_epi8(_mm256_cmpeq_epi8(input[3], mask)),
+            _mm256_movemask_epi8(_mm256_cmpeq_epi8(input[2], mask)),
+            _mm256_movemask_epi8(_mm256_cmpeq_epi8(input[1], mask)),
+            _mm256_movemask_epi8(_mm256_cmpeq_epi8(input[0], mask)),
+        )
+    };
+
+    // let starts = backslashes & (!(backslashes << 1));
+    // there is no slli for si256, have to split into 64 bit values
+    let starts = unsafe {
+        _mm256_andnot_si256(_mm256_slli_epi64(backslashes, 1), backslashes)
+    };
+
+    unimplemented!()
 }

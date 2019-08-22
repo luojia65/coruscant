e3948f958e39865bbfd674ae36c39f4f2de1956a
luojia65 <me@luojia.cc>
2019-08-22 13:25:55 +0800

    Carries; todo

--- coruscant-tyjson/examples/m256x8-m256-input.rs
+++ coruscant-tyjson/examples/m256x8-m256-input.rs
@@ -22,6 +22,7 @@ fn main() {
 fn odd_backslash_sequences(input: [__m256i; 8], prev_ov: &mut bool) -> __m256i {
     let even_bits = unsafe { _mm256_set1_epi8(transmute(0x55u8)) };
     let odd_bits = unsafe { _mm256_set1_epi8(transmute(0xAAu8)) };
+    let one = unsafe { _mm256_set_epi64x(0, 0, 0, 1) };
 
     let backslashes = unsafe {
         let mask = _mm256_set1_epi8(b'\\' as i8);
@@ -36,7 +37,6 @@ fn odd_backslash_sequences(input: [__m256i; 8], prev_ov: &mut bool) -> __m256i {
             _mm256_movemask_epi8(_mm256_cmpeq_epi8(input[0], mask)),
         )
     };
-    print_m256(backslashes);
 
     // let starts = backslashes & (!(backslashes << 1));
     let starts = unsafe {
@@ -49,19 +49,32 @@ fn odd_backslash_sequences(input: [__m256i; 8], prev_ov: &mut bool) -> __m256i {
         // calculate backslashes & (!ans_sll)
         _mm256_andnot_si256(ans_sll, backslashes)
     };
-    print_m256(starts);
 
     let even_start_flipped_mask = if *prev_ov { 
-        let one = unsafe { _mm256_set_epi64x(0, 0, 0, 1) };
         unsafe { _mm256_xor_si256(odd_bits, one) }
     } else { 
         odd_bits  
     };
     let even_starts = unsafe { _mm256_andnot_si256(even_start_flipped_mask, starts) };
     let odd_starts = unsafe { _mm256_and_si256(even_start_flipped_mask, starts) };
+    // this 64-bit add would not overflow; all the odd bits are already zero
     let even_carries = unsafe { _mm256_add_epi64(even_starts, backslashes) };
+    let (odd_carries, ov) = unsafe { 
+        let add64 = _mm256_add_epi64(odd_starts, backslashes);
+        let ov64_hi = _mm256_and_si256(odd_starts, backslashes);
+        let ov64_inplace = _mm256_srli_epi64(ov64_hi, 63);
+        let ov64_rol = _mm256_permute4x64_epi64(ov64_inplace, 0b10_01_00_11);
+        let ov256 = _mm256_cvtsi256_si32(ov64_rol);
+        let ov64_rol = _mm256_insert_epi64(ov64_rol, 0, 0);
+        let add256 = _mm256_add_epi64(add64, ov64_rol);
+        (add256, ov256)
+    };
+    let odd_carries = if *prev_ov { 
+        unsafe { _mm256_or_si256(odd_carries, one) }
+    } else {
+        odd_carries
+    };
     
-
     unimplemented!()
 }
 
